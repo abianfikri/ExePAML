5d4352bd75b6d4b45b431174732404e6dff65795
abianfikri <abronjoy46@gmail.com>
2023-04-02 00:00:39 +0700

    membuat tampilan loginPage dan backendnya

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,3 +1,4 @@
+import 'package:exercise_paml1/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -10,8 +11,161 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _fieldData = GlobalKey<FormState>();
+  final emailData = TextEditingController();
+  final passData = TextEditingController();
+
+  bool eyeToggle = true;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Form(
+              key: _fieldData,
+              child: Column(
+                children: [
+                  Padding(padding: EdgeInsets.all(60)
+                  ),
+                  Image.asset(
+                    'assets/image/pokemon.png',
+                    width: 300,
+                  ),
+                  Padding(padding: EdgeInsets.all(10)
+                  ),
+                  Text(
+                    "Let's get started",
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                    )),
+                    Padding(padding: EdgeInsets.all(10)
+                    ),
+                  Text(
+                    "There's No Sense In Going Out of",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16
+                    ),
+                  ),
+                  Text(
+                    "Your Way To Get Somebody To Like You.",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.all(30)
+                  ),
+                  Container(
+                    width: 400,
+                    height: 80,
+                    child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: emailData,
+                      decoration: InputDecoration(
+                        labelText: "Name",
+                        hintText: "Enter your name",
+                        prefixIcon: Icon(Icons.person)
+                    ),
+                    validator: (value) {
+                      bool valid = RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value!);
+
+                      if (value.isEmpty) {
+                        return "Please enter your name";
+                      }
+                    },
+                    ),
+                  ),
+                  Container(
+                    width: 400,
+                    height: 100,
+                    child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: eyeToggle,
+                    controller: passData,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      hintText: "Enter your password",
+                      prefixIcon: Icon(Icons.lock),
+                      suffix: InkWell(
+                        onTap: () {
+                          setState(() {
+                            eyeToggle = !eyeToggle;
+                          });
+                        },
+                        child: Icon(
+                            eyeToggle ? Icons.visibility : Icons.visibility_off),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please enter your password";
+                      } else if (passData.text.length < 6) {
+                        return "Please must be atleast 6 characters";
+                      }
+                    },
+                )
+                ),
+                InkWell(
+                  onTap: () {
+                    if (_fieldData.currentState!.validate()) {
+                      print("Success");
+                      emailData.clear();
+                      passData.clear();
+                      Navigator.push(context, 
+                      MaterialPageRoute(builder:  (context) => MyApp()));
+                    }
+                  },
+                  child: Container(
+                    width: 300,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Center(
+                        child: Text(
+                      "Sign In",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account? ",
+                        style: TextStyle(
+                          color: Colors.grey
+                        ),
+                      ),
+                      TextButton(onPressed: (){
+
+                      }, child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: Colors.amber
+                        ),
+                      ))
+                    ],
+                  )
+                )
+                ],
+              ),)),
+        )
+    );
   }
 }
